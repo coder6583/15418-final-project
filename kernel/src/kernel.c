42b6a4cb4358cde2aeb91fdecde1365bf5e499f0
Soma Narita <thedude6583@gmail.com>
2026-04-15 01:47:50 -0400

    first spi test

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -13,6 +13,7 @@
 #include <exti.h>
 #include <lcd_driver.h>
 #include "systick.h"
+#include "spi.h"
 
 #define UART_USARTDIV_MANTISSA 8
 #define UART_USARTDIV_FRAC 11
@@ -83,42 +84,52 @@ int kernel_main(void) {
     uart_init(UART_USARTDIV);
     enable_fpu();
 
-    struct pin left_motor_in1 = {.port=MOTORA_IN1_PORT, .num=MOTORA_IN1_NUM, .irq_num=0};
-    struct pin left_motor_in2 = {.port=MOTORA_IN2_PORT, .num=MOTORA_IN2_NUM, .irq_num=0};
-    struct pin left_motor_en = {.port=MOTORA_EN_PORT, .num=MOTORA_EN_NUM, .irq_num=0};
-    struct motor_timer left_timer = {.timer=MOTORA_TIMER, .channel=MOTORA_CHN, 
-	                             .gpio_alt=MOTORA_ALT, .is_comp=0};
-    struct motor_attr left_motor = {.motor_in1=left_motor_in1, .motor_in2=left_motor_in2,
-                                    .motor_en=left_motor_en, .timer=left_timer};
+    // struct pin left_motor_in1 = {.port=MOTORA_IN1_PORT, .num=MOTORA_IN1_NUM, .irq_num=0};
+    // struct pin left_motor_in2 = {.port=MOTORA_IN2_PORT, .num=MOTORA_IN2_NUM, .irq_num=0};
+    // struct pin left_motor_en = {.port=MOTORA_EN_PORT, .num=MOTORA_EN_NUM, .irq_num=0};
+    // struct motor_timer left_timer = {.timer=MOTORA_TIMER, .channel=MOTORA_CHN, 
+	//                              .gpio_alt=MOTORA_ALT, .is_comp=0};
+    // struct motor_attr left_motor = {.motor_in1=left_motor_in1, .motor_in2=left_motor_in2,
+    //                                 .motor_en=left_motor_en, .timer=left_timer};
 
-    struct pin right_motor_in1 = {.port=MOTORB_IN1_PORT, .num=MOTORB_IN1_NUM, .irq_num=0};
-    struct pin right_motor_in2 = {.port=MOTORB_IN2_PORT, .num=MOTORB_IN2_NUM, .irq_num=0};
-    struct pin right_motor_en = {.port=MOTORB_EN_PORT, .num=MOTORB_EN_NUM, .irq_num=0};
-    struct motor_timer right_timer = {.timer=MOTORB_TIMER, .channel=MOTORB_CHN, 
-	                             .gpio_alt=MOTORB_ALT, .is_comp=0};
-    struct motor_attr right_motor = {.motor_in1=right_motor_in1, .motor_in2=right_motor_in2,
-                                    .motor_en=right_motor_en, .timer=right_timer};
+    // struct pin right_motor_in1 = {.port=MOTORB_IN1_PORT, .num=MOTORB_IN1_NUM, .irq_num=0};
+    // struct pin right_motor_in2 = {.port=MOTORB_IN2_PORT, .num=MOTORB_IN2_NUM, .irq_num=0};
+    // struct pin right_motor_en = {.port=MOTORB_EN_PORT, .num=MOTORB_EN_NUM, .irq_num=0};
+    // struct motor_timer right_timer = {.timer=MOTORB_TIMER, .channel=MOTORB_CHN, 
+	//                              .gpio_alt=MOTORB_ALT, .is_comp=0};
+    // struct motor_attr right_motor = {.motor_in1=right_motor_in1, .motor_in2=right_motor_in2,
+    //                                 .motor_en=right_motor_en, .timer=right_timer};
 
-    struct pin left_encoder_a = {.port=ENCA_L_PORT, .num=ENCA_L_NUM, .irq_num=EXTI1_IRQ};
-    struct pin left_encoder_b = {.port=ENCB_L_PORT, .num=ENCB_L_NUM, .irq_num=EXTI0_IRQ};
-    struct pin right_encoder_a = {.port=ENCA_R_PORT, .num=ENCA_R_NUM, .irq_num=EXTI9_5_IRQ};
-    struct pin right_encoder_b = {.port=ENCB_R_PORT, .num=ENCB_R_NUM, .irq_num=EXTI9_5_IRQ};
-    struct encoder_pin_attr left_encoder = {.encoder_pin_a=left_encoder_a,
-                                            .encoder_pin_b=left_encoder_b};
-    struct encoder_pin_attr right_encoder = {.encoder_pin_a=right_encoder_a,
-                                             .encoder_pin_b=right_encoder_b};
+    // struct pin left_encoder_a = {.port=ENCA_L_PORT, .num=ENCA_L_NUM, .irq_num=EXTI1_IRQ};
+    // struct pin left_encoder_b = {.port=ENCB_L_PORT, .num=ENCB_L_NUM, .irq_num=EXTI0_IRQ};
+    // struct pin right_encoder_a = {.port=ENCA_R_PORT, .num=ENCA_R_NUM, .irq_num=EXTI9_5_IRQ};
+    // struct pin right_encoder_b = {.port=ENCB_R_PORT, .num=ENCB_R_NUM, .irq_num=EXTI9_5_IRQ};
+    // struct encoder_pin_attr left_encoder = {.encoder_pin_a=left_encoder_a,
+    //                                         .encoder_pin_b=left_encoder_b};
+    // struct encoder_pin_attr right_encoder = {.encoder_pin_a=right_encoder_a,
+    //                                          .encoder_pin_b=right_encoder_b};
 
-    motor_init(LEFT_MOTOR, &left_motor, &left_encoder);
-    motor_init(RIGHT_MOTOR, &right_motor, &right_encoder);
+    // motor_init(LEFT_MOTOR, &left_motor, &left_encoder);
+    // motor_init(RIGHT_MOTOR, &right_motor, &right_encoder);
 
-    gpio_init(BUTTON1_PORT, BUTTON1_NUM, MODE_INPUT, OUTPUT_PUSH_PULL, 
-	      OUTPUT_SPEED_HIGH, PUPD_PULL_UP, ALT0);
-    enable_exti(BUTTON1_PORT, BUTTON1_NUM, RISING_EDGE);
-    gpio_init(BUTTON2_PORT, BUTTON2_NUM, MODE_INPUT, OUTPUT_PUSH_PULL, 
-	      OUTPUT_SPEED_HIGH, PUPD_PULL_UP, ALT0);
-    enable_exti(BUTTON2_PORT, BUTTON2_NUM, RISING_EDGE);
+    // gpio_init(BUTTON1_PORT, BUTTON1_NUM, MODE_INPUT, OUTPUT_PUSH_PULL, 
+	//       OUTPUT_SPEED_HIGH, PUPD_PULL_UP, ALT0);
+    // enable_exti(BUTTON1_PORT, BUTTON1_NUM, RISING_EDGE);
+    // gpio_init(BUTTON2_PORT, BUTTON2_NUM, MODE_INPUT, OUTPUT_PUSH_PULL, 
+	//       OUTPUT_SPEED_HIGH, PUPD_PULL_UP, ALT0);
+    // enable_exti(BUTTON2_PORT, BUTTON2_NUM, RISING_EDGE);
     
-    enter_user_mode();
+    // enter_user_mode();
+
+    sys_spi_init(LINK_IN);
+    sys_spi_init(LINK_OUT);
+
+    char *s = "hello";
+    char recv[6];
+    recv[5] = '\0';
+
+    sys_spi_receive((uint8_t*)recv, 5);
+
     while (1);
     return 0;
 }
